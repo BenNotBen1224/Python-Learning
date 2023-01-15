8282bfdbd9b033f2df099c2f416878034850b5ba
Mr.Ben <1145569313@qq.com>
2023-01-14 22:23:27 -0800

    1/14/2023第二次

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -9,25 +9,34 @@ class TicTacToe:
     
     def __init__(self):
         # 创造一个3x3的二维向量
-        self.board = [['-' for j in range(3)] for i in range(3)]     
-    
+        self.board = [['-' for j in range(3)] for i in range(3)]
+        
+    def Print_board(self):
+        # print([[(j, sep=(' ')) for j in row] for i in self.board)
+            # 为什么不行？
+            # sep 似乎只能在纯print里使用，不能这么操作。而且，你要用两个[],你这里调动了两个loop。最后，这种操作最好是define的时候用，print就别偷懒了
+        for vertical in self.board:
+                for horizontal in vertical:
+                    print(horizontal, end=" ")
+                print("\n")
+            
     def Check_board_win(self, player):
         #检查横竖斜是否成功
         if [[self.board[i][j] != player or self.board[j][i] != player or 
              self.board[i][i] != player or self.board[i][3 - i] 
              for j in range(3)] for i in range(3)]:
-            win = True
+            True
         else:
-            win = False
+            False
             
     def Check_board_filled(self):
         #检查棋子是否满盘
         if[[self.board[i][j] != '-' for j in range[3]] for i in range[3]]:
-            win = True
+            True
         else:
-            win = False
-    
-    def Game(self, x, player):
+            False
+               
+    def Game(self):
         """
         印出九宫格
         选择玩家
@@ -41,36 +50,24 @@ class TicTacToe:
         赢/输
         上局未开头玩家下局开头
         """
+        self.board = [['-' for j in range(3)] for i in range(3)]
+        self.Print_board()
         print("Welcome to Game")
         
         while True:
-            # print([[(j, sep=(' ')) for j in row] for i in self.board)
-            #为什么不行？
-            # sep 似乎只能在纯print里使用，不能这么操作。而且，你要用两个[],你这里调动了两个loop。最后，这种操作最好是define的时候用，print就别偷懒了
-            # 你是想这样做？
-            for i in self.board:
-                for j in self.board:
-                    print(self.board[i][j], end=" ")
-                print("\n")
-            
-        
-            #随机选玩家并提示下棋
-            if random.randint(0,1) == 0: 
-                player = "X" 
-            else: 
-                player = "O"
-            print(f"Player {player} turn")
-        
-            ##上传玩家输出
+            #随机选玩家并提示输入
+            player = "X" if random.randint(0, 1) == 0 else "O"
+            x = list((map(int, input(f"Player {player} turn" + "(please type in x and y position using comma in between):").split(","))))
             self.board[x[0]][x[1]] = player
             
-            #检查结果
-            self.Check_board()
+            ##上传玩家输出
+            self.Print_board()
             
+            #检查结果
+            self.Check_board_win(player)
             if self.Check_board_win(player):
                 print(f"Congradulations Player {player}, you win the Game!")
                 False
-        
             if self.Check_board_filled():
                 print("Draw \n Board is filled")
                 False
@@ -79,6 +76,7 @@ class TicTacToe:
             # player = "X" if player == "O" else player = "O"
             # 应该这样做
             player = "X" if player == "O" else "O"
+            
 
 A_Game = TicTacToe()
 A_Game.Game()
